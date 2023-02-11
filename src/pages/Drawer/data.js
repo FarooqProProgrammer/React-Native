cdcd59faba4e251beb5777eb1479e7c9e5318d68
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-02-11 10:41:13 -0800

    Create data.js

---
+++ src/pages/Drawer/data.js
@@ -0,0 +1,25 @@
+const DATA = [
+    {
+        id:1,
+        title:"Coffee",
+        discription:"loremdasdasfsdgsdg"
+    },
+    {
+        id:2,
+        title:"Coffee",
+        discription:"loremdasdasfsdgsdg"
+    },
+    {
+        id:3,
+        title:"Coffee",
+        discription:"loremdasdasfsdgsdg"
+    },
+    {
+        id:4,
+        title:"Coffee",
+        discription:"loremdasdasfsdgsdg"
+    },
+    
+]
+
+export default DATA
